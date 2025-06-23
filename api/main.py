2f38a9b2807a09dd61c6625f8713b4fe2eda0ae4
Caio <129826533+dib10@users.noreply.github.com>
2025-06-23 16:45:39 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 # api/main.py
 from fastapi import FastAPI
-from api.endpoints import wishlist  # Importe o módulo do endpoint
+from api.endpoints import wishlist 
 
 app = FastAPI(title="Amazon Wishlist Scraper API")
 
-# Inclua o router do nosso endpoint no app principal
 app.include_router(wishlist.router, prefix="/api", tags=["Wishlist"])
 
 @app.get("/")
